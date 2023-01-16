c101fd41c841f16cf6e6ed8f0cced5d398ffb150
Saifur Rahman Mohsin <mohsin92@me.com>
2023-01-16 15:17:45 +0530

    feat: Migrate theme to Nuxt

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Html>
+    <Body class="is-preload">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, user-scalable=no',
+      title: 'Strongly Typed by HTML5 UP',
+      meta: [
+        { name: 'description', content: 'Strongly Typed theme by HTML5Up' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'css/main.css' }
+      ],
+      script: [
+        { type: 'text/javascript', src: 'js/jquery.min.js', body: true },
+        { type: 'text/javascript', src: 'js/browser.min.js', body: true },
+        { type: 'text/javascript', src: 'js/breakpoints.min.js', body: true },
+        { type: 'text/javascript', src: 'js/util.js', body: true },
+        { type: 'text/javascript', src: 'js/main.js', body: true }
+      ]
+    },
+  },
 })
